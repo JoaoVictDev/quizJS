918de6d020fef5017d3f3a4f9de602c258689f66
Victor <santosjoao.dev@gmail.com>
2021-01-29 20:29:11 -0200

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizContainer from '../src/components/QuizContainer'
+import AlternativesForm from '../src/components/AlternativesForm'
+import BackLinkArrow from '../src/components/BackLinkArrow'
+import Button from '../src/components/Button'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Input from '../src/components/Input'
+import Link from '../src/components/Link'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of CSS</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Lorem ipsummmm dolor sit amet</p>
+          </Widget.Content>
+        </Widget>
+        
+        <Widget>
+    
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>Lorem ipsummmm dolor sit amet</p>
+          </Widget.Content>
+
+        </Widget>
+        <Footer />
+
+      </QuizContainer>
+      <GitHubCorner projectUrl= "https://github.com/joaoVictDev"/>
+    </QuizBackground>
+  )
 }
